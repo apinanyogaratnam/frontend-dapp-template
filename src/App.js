a6328246d8012240735e93f01cb51bda824f2312
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-11-01 20:31:40 -0400

    created components for metamask connection

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEthers } from '@usedapp/core';
 
 function App() {
+  const { account, accessBrowserWallet } = useEthers();
+  const isConnected = account !== undefined;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DAppProvider config={{
+      supportedChains: [ChainId.Mainnet]
+    }}>
+      <div className="App">
+        <header className="App-header">
+          {isConnected ? <button>Connected!</button> : <button onClick={accessBrowserWallet}>Connect</button>}
+        </header>
+      </div>
+    </DAppProvider>
   );
 }
 
