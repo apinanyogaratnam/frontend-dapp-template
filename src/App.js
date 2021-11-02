4650de43091fa21ba85d1e92b4c263b688afeb80
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-11-01 20:40:02 -0400

    changing connection to metamask into a seperate component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEthers, DAppProvider, ChainId } from '@usedapp/core';
+import { DAppProvider, ChainId } from '@usedapp/core';
+import { Header } from './components/Header';
 
 function App() {
-  const { account, accessBrowserWallet } = useEthers();
-  const isConnected = account !== undefined;
-
   return (
     <DAppProvider config={{
       supportedChains: [ChainId.Mainnet]
     }}>
       <div className="App">
         <header className="App-header">
-          {isConnected ? <button>Connected!</button> : <button onClick={accessBrowserWallet}>Connect</button>}
+          <Header />
         </header>
       </div>
     </DAppProvider>
